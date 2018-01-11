975472849d8f085da6d8a68edefd9ffb5e9688a0
Abhishek Chaudhary <acechd97@gmail.com>
2018-01-12 04:04:58 +0530

    Added scoring and wickets logic

--- app/src/main/java/com/example/abhishek/cricketscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/abhishek/cricketscorekeeper/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.abhishek.cricketscorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scoreTeamA = 0;
+        (findViewById(R.id.team_a_ball_1)).setBackgroundResource(R.color.colorSuccess);
     }
 
     /**
@@ -74,38 +75,63 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayOverScore(int s) {
         String displayScore;
+        TextView currentView = null;
+        TextView swap = null;
         if (s == -1)
             displayScore = "O";
         else
             displayScore = "" + s;
         switch (ball) {
             case 1:
-                ((TextView) findViewById(R.id.team_a_ball_1)).setText(displayScore);
+                currentView = findViewById(R.id.team_a_ball_1);
+                findViewById(R.id.team_a_ball_2).setBackgroundResource(R.color.colorSuccess);
                 break;
             case 2:
-                ((TextView) findViewById(R.id.team_a_ball_2)).setText(displayScore);
+                currentView = findViewById(R.id.team_a_ball_2);
+                findViewById(R.id.team_a_ball_3).setBackgroundResource(R.color.colorSuccess);
                 break;
             case 3:
-                ((TextView) findViewById(R.id.team_a_ball_3)).setText(displayScore);
+                currentView = findViewById(R.id.team_a_ball_3);
+                findViewById(R.id.team_a_ball_4).setBackgroundResource(R.color.colorSuccess);
                 break;
             case 4:
-                ((TextView) findViewById(R.id.team_a_ball_4)).setText(displayScore);
+                currentView = findViewById(R.id.team_a_ball_4);
+                findViewById(R.id.team_a_ball_5).setBackgroundResource(R.color.colorSuccess);
                 break;
             case 5:
-                ((TextView) findViewById(R.id.team_a_ball_5)).setText(displayScore);
+                currentView = findViewById(R.id.team_a_ball_5);
+                findViewById(R.id.team_a_ball_6).setBackgroundResource(R.color.colorSuccess);
                 break;
             case 6:
-                ((TextView) findViewById(R.id.team_a_ball_6)).setText(displayScore);
+                currentView = findViewById(R.id.team_a_ball_6);
                 break;
             case 7:
-                ((TextView) findViewById(R.id.team_a_ball_1)).setText(displayScore);
-                ((TextView) findViewById(R.id.team_a_ball_2)).setText("0");
-                ((TextView) findViewById(R.id.team_a_ball_3)).setText("0");
-                ((TextView) findViewById(R.id.team_a_ball_4)).setText("0");
-                ((TextView) findViewById(R.id.team_a_ball_5)).setText("0");
-                ((TextView) findViewById(R.id.team_a_ball_6)).setText("0");
+                currentView = findViewById(R.id.team_a_ball_1);
+                swap = findViewById(R.id.team_a_ball_2);
+                swap.setText("0");
+                swap.setBackgroundResource(R.color.colorSuccess);
+                swap = findViewById(R.id.team_a_ball_3);
+                swap.setText("0");
+                swap.setBackgroundResource(R.color.colorGray300);
+                swap = findViewById(R.id.team_a_ball_4);
+                swap.setText("0");
+                swap.setBackgroundResource(R.color.colorGray300);
+                swap = findViewById(R.id.team_a_ball_5);
+                swap.setText("0");
+                swap.setBackgroundResource(R.color.colorGray300);
+                swap = findViewById(R.id.team_a_ball_6);
+                swap.setText("0");
+                swap.setBackgroundResource(R.color.colorGray300);
                 ball = 1;
+                swap = findViewById(R.id.team_a_over);
+                swap.setText(String.format("%d", Integer.parseInt(swap.getText().toString()) + 1));
         }
+        currentView.setText(displayScore);
+        if (displayScore.equalsIgnoreCase("o")) {
+            currentView.setBackgroundResource(R.color.colorDanger);
+            swap = findViewById(R.id.team_a_wickets);
+            swap.setText(String.format("%d", Integer.parseInt(swap.getText().toString()) + 1));
+        } else currentView.setBackgroundResource(R.color.colorGray500);
         ball++;
     }
 
